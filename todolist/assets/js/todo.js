9e316443b0a635bbc1200caef8a4c5be3cf5e42c
Sohail Mumtaz <53467308+sohailmumtaz123@users.noreply.github.com>
2022-09-20 18:44:51 +0500

    Add files via upload

---
+++ todolist/assets/js/todo.js
@@ -0,0 +1,24 @@
+// alert("Connected");
+// $("li").click(function(){
+// 	$(this).toggleClass("completed");
+	
+// });
+$("ul").on("click", "span", function(e){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+		console.log("Span Clicked");
+	});
+	event.stopPropagation();
+});
+
+$("input[type=text]").keypress(function(event){
+	if (event.which===13) {
+		var todoText=$(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>" )
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type=text").fadeToggle();
+});
